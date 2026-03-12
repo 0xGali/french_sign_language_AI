65f053b5e8018865f235114acef8ae5b659c18a2
Maximilien Bleeker <maximilien.bleeker9@etu.univ-lorraine.fr>
2026-03-12 16:51:45 +0100

    Ajout de documentation, limite de taille

--- handTracking.py
+++ handTracking.py
@@ -59,30 +59,81 @@ def sp_noise(image, prob):
 
 def lectureVideo():
     mapMot = {}
-    compteur = 0
+
+    #On parcourt le dossier "videos" pour récupérer les vidéos à traiter
     for video in os.listdir(f"videos"):
         listeMain = []
         print(video)
-        if compteur == 20:
-            break
         cap = cv2.VideoCapture(f"videos/{video}")
         detector = handDetector()
+
+        #La boucle continue tant que la vidéo est lancée
         while True and cap.isOpened():
+
+            #On récupère une image de la vidéo
             success, img= cap.read()
             if img is None:
                 break
+
+            #Redimentionnement de l'image pour accélérer le processus de détection
+            if img.shape[0] > 300:
+                img = cv2.resize(img, (int(img.shape[1]//1.5), int(img.shape[0]//1.5)))
+
+            #Détection des mains sur l'image et l'affiche sur l'image
             img = detector.findHands(img)
+
+            #En fonction du nombre de mains détectées, on récupère les coord et on les stocke dans 1 ou 2 listes
             if detector.results.multi_hand_landmarks == None:
                 continue
             elif len(detector.results.multi_hand_landmarks) == 1:
                 lmList = detector.findPosition(img,0)
-                print(lmList)
                 listeMain.append(lmList)
             elif len(detector.results.multi_hand_landmarks) == 2:
                 lmList = detector.findPosition(img,0)
                 lmList2 = detector.findPosition(img,1)
-                print(lmList)
-                print(lmList2)
+                listeMain.append((lmList,lmList2))
+            
+            #Affiche les images
+            #cv2.imshow("Image",img)
+
+            #Permet de finir la lecture de la vidéo en appuyant sur "q"
+            if cv2.waitKey(1) == ord('q'):
+                break
+        
+        video = video.split(".")[0]
+        #Crée un map en associant le mot à la liste de coordonnées des mains
+        mapMot.update({video:listeMain})
+    return mapMot
+
+def lectureVideoBruit():
+    mapMot = {}
+
+    for video in os.listdir(f"videos"):
+        listeMain = []
+        print(video)
+        cap = cv2.VideoCapture(f"videos/{video}")
+        detector = handDetector()
+
+        while True and cap.isOpened():
+            success, img= cap.read()
+            if img is None:
+                break
+
+            if img.shape[0] > 300:
+                img = cv2.resize(img, (int(img.shape[1]//1.5), int(img.shape[0]//1.5)))
+
+            #On applique du bruit à l'image avant de faire la détection des mains
+            img = sp_noise(img, 0.01)
+            img = detector.findHands(img)
+
+            if detector.results.multi_hand_landmarks == None:
+                continue
+            elif len(detector.results.multi_hand_landmarks) == 1:
+                lmList = detector.findPosition(img,0)
+                listeMain.append(lmList)
+            elif len(detector.results.multi_hand_landmarks) == 2:
+                lmList = detector.findPosition(img,0)
+                lmList2 = detector.findPosition(img,1)
                 listeMain.append((lmList,lmList2))
             
             #cv2.imshow("Image",img)
@@ -90,52 +141,14 @@ def lectureVideo():
                 break
         video = video.split(".")[0]
         mapMot.update({video:listeMain})
-        compteur += 1
-    return mapMot
-
-def lectureVideoBruit():
-    mapMot = {}
-    compteur = 0
-    for video in os.listdir(f"videos"):
-        listeMain = []
-        print(video)
-        if compteur == 20:
-            break
-        cap = cv2.VideoCapture(f"videos/{video}")
-        detector = handDetector()
-        while True and cap.isOpened():
-            success, img= cap.read()
-            if img is None:
-                break
-            img = sp_noise(img, 0.01)
-            img = detector.findHands(img)
-            if detector.results.multi_hand_landmarks == None:
-                continue
-            elif len(detector.results.multi_hand_landmarks) == 1:
-                lmList = detector.findPosition(img,0)
-                print(lmList)
-                listeMain.append(lmList)
-            elif len(detector.results.multi_hand_landmarks) == 2:
-                lmList = detector.findPosition(img,0)
-                lmList2 = detector.findPosition(img,1)
-                print(lmList)
-                print(lmList2)
-                listeMain.append((lmList,lmList2))
-            
-            cv2.imshow("Image",img)
-            if cv2.waitKey(1) == ord('q'):
-                break
-        video = video.split(".")[0]
-        mapMot.update({video:listeMain})
-        compteur += 1
     return mapMot
 
 def main():
     mapMot = lectureVideo()
     mapMotBruit = lectureVideoBruit()
     mapFusion = {**mapMot, **mapMotBruit}   
-    print(mapMot)
-    print(mapMotBruit)
+
+    #On crée le json à partir de la fusion des 2 maps (avec et sans bruit)
     with open("coordonnees_mots.json", "w") as f:
         json.dump(mapFusion, f)
 
